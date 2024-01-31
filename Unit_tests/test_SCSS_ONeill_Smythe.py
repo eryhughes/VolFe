93cf178dc9d47187e322c27f23232e8f33c2276d
PennyWieser <penny.wieser@gmail.com>
2024-01-30 16:06:04 -0800

    added more to gitignore

---
+++ Unit_tests/test_SCSS_ONeill_Smythe.py
@@ -0,0 +1,21 @@
+import unittest
+import pandas as pd
+import VolFe as vf
+
+## Setting a liquid composition
+
+
+
+class test_mantlemelt(unittest.TestCase):
+    def test_Simple_mantle_Cu(self):
+        self.assertAlmostEqual(2+2, 4,
+decimalPlace, "Inst Cu doesnt match test value")
+
+
+
+
+
+
+
+if __name__ == '__main__':
+     unittest.main()
