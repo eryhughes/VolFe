2219793585fb07267c67e6b66f71f6fdb14683d4
eryhughes <64137982+eryhughes@users.noreply.github.com>
2026-08-05 13:57:19 +0100

    convert wood group CS6+ to ppmw

--- src/VolFe/model_dependent_variables.py
+++ src/VolFe/model_dependent_variables.py
@@ -1881,7 +1881,7 @@ def C_SO4(PT, melt_wf, models=default_models):
         )
         if model == "Boulliung23_eq9_12":
             logC = logC + (((PT["P"] - 1) * 29.2) / (8.314 * 2.303 * T))
-        Csulfate = 10.0 ** (logC)
+        Csulfate = (10.0 ** (logC))*1.e5 # convert wt% to ppmw
 
     # Eq. (11) from Boulliung, J., Wood, B.J. Sulfur oxidation state and solubility in
     # silicate melts. Contrib Mineral Petrol 178, 56 (2023).
@@ -1908,7 +1908,7 @@ def C_SO4(PT, melt_wf, models=default_models):
             )
             + 55.029 * math.log10(T)
         )
-        Csulfate = 10.0 ** (logC)
+        Csulfate = (10.0 ** (logC))*1.e5 # convert wt% to ppmw
 
     # Eq. (21) from Thomas, R.W. and Wood, B.J., 2026. Sulfur speciation in silicate melts at high pressure. Geochimica et Cosmochimica Acta.
     # 417:37-51 https://doi.org/10.1016/j.gca.2026.02.003
@@ -1935,7 +1935,7 @@ def C_SO4(PT, melt_wf, models=default_models):
             )
             + 55.03 * math.log10(T)
         )
-        Csulfate = 10.0 ** (logC)
+        Csulfate = (10.0 ** (logC))*1.e5 # convert wt% to ppmw
 
     # Eq. (22) from Thomas, R.W. and Wood, B.J., 2026. Sulfur speciation in silicate melts at high pressure. Geochimica et Cosmochimica Acta.
     # 417:37-51 https://doi.org/10.1016/j.gca.2026.02.003
@@ -1959,7 +1959,7 @@ def C_SO4(PT, melt_wf, models=default_models):
             )
             / T
         )
-        Csulfate = 10.0 ** (logC)
+        Csulfate = (10.0 ** (logC))*1.e5 # convert wt% to ppmw
 
     # Eq. (10) or (11) with/without (14) from Gorojovsky, L.R. and Wood, B.J., (2026). Solubility and speciation of sulfur in silicate melts under crustal conditions.
     # EPSL 687:120088 https://doi.org/10.31223/X5T755
@@ -2013,7 +2013,7 @@ def C_SO4(PT, melt_wf, models=default_models):
         # Pressure-term from Thomas & Wood (2026)
         if model in ["Gorojovsky26_eq10_14", "GorojovskyPP_eq26_14"]:
             logC = logC - ((PT["P"] * 0.165) / T)
-        Csulfate = 10.0 ** (logC)
+        Csulfate = (10.0 ** (logC))*1.e5 # convert wt% to ppmw
 
     # OLD #
     # elif model == "Nash19": # Nash et al. (2019) EPSL 507:187-198
